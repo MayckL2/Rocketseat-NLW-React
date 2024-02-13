ec5ddd50f3cb11cc0153c7fd4189c8e3438ebf6a
mayckl2 <mayckluciano2@gmail.com>
2024-02-13 20:04:54 -0300

    :bug: Importações não utilizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import Logo from './assets/Logo.png'
 import { CardNote } from './components/cardNote'
 import { NewNote } from './components/newNote'
